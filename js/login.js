f5f15d9244a788ae05d9210304897e68c543d213
yuli <1963886482@qq.com>
2018-05-12 17:59:52 +0800

    change

---
+++ js/login.js
@@ -0,0 +1,17 @@
+$(function (){
+	$(".box").focus(function(){
+		$(this).parent().css("border-color","red");
+	}).blur(function(){
+		$(this).parent().css("border-color","#999999");
+	});
+	$("#btn").click(function(e){
+		//console.log(e);
+		$.post("http://www.ftusix.com/static/data/login.php",{
+			mobile:$("#mobile").val(),
+			pwd:$("#pwd").val()
+		},function(data,textStatus){
+			console.log(data);
+		})
+
+	})
+});
